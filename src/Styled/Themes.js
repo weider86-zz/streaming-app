128847f583a3a2126f23aa6e5db2410b11f6be26
Weider Lima <weider.lima@mercadolivre.com>
2020-07-20 22:31:37 -0300

    Add Theme and global styles

---
+++ src/Styled/Themes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+export const lightTheme = {
+  body: '#E2E2E2',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  gradient: 'linear-gradient(#39598A, #79D7ED)',
+}
+
+export const darkTheme = {
+  body: '#363537',
+  text: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  gradient: 'linear-gradient(#091236, #1E215D)',
+}
+
+export const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+)
