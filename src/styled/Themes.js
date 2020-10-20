e799c7888507211379094574349834dd9df85a92
Weider Lima <weider.lima@mercadolivre.com>
2020-10-20 16:42:30 -0300

    Add more theme variables

--- src/styled/Themes.js
+++ src/styled/Themes.js
@@ -5,22 +5,27 @@ import variables from './Andes/_variables.scss'
 const lightColor = variables.andesGray70
 const darkColor = variables.andesGray800
 const whiteColor = variables.andesWhite
+const blackColor = variables.andesBlack
+const pink = '#ff008e'
 
 const basicTheme = {
   fontFamily: variables.fontFamily,
   fontSize: variables.fontSizeBase,
+  brandColor: pink,
 }
 
 export const lightTheme = {
   ...basicTheme,
   bodyBg: lightColor,
-  textColor: '#3b074c',
+  textColor: blackColor,
+  hardColor: whiteColor,
 }
 
 export const darkTheme = {
   ...basicTheme,
   bodyBg: darkColor,
   textColor: whiteColor,
+  hardColor: blackColor,
 }
 
 export const Theme = ({ children }) => (
